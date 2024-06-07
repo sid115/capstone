793280e7935169621845748e13ba7d982dc2648f
marrk2 <tobias_dennis.magh@smail.th-koeln.de>
2024-06-07 14:13:03 +0200

    Userfriendly lut generation with pv-lut script. Changed lut in plecs for correct temperature behavior

--- src/pv_lut.m
+++ src/pv_lut.m
@@ -1,32 +1,48 @@
-%%% VARIABLES %%%
+%%% PV LUT GENERATION %%%
+% This script generates a lookup table for a PV module based
+% on the mathematical model of U.Boeke publiched in
+% "A simple model of photovoltaic module electric characteristics".
 
-tn = 25; % nominal temperature in °C
-astc = 1000; % solar radiation at STC in W/m^2
-b = 0.09; % fit parameter
-vstep = 101; % number of voltage steps
-astep = 11; % number of radiation steps
-tstep = 21;
+% USAGE:
+% 1. Set the parameters in the "PV module data" section below
+% 2. Run the script with Octave or Matlab (tested with Octave 7.3.0)
+% 3. The lookup table is saved in the "out" folder as "pv_lut.mat"
+% (4. Adjust the fit parameter "b" under "Lut Variables" according to the instructions in the Boeke Paper and rerun the script)
 
-% PV module data
-voc = 86.4; % open circuit voltage in V
-isc = 6.14; % short circuit current in A
-vmpp = 70.4; % voltage at maximum power point in V
-impp = 5.58; % current at maximum power point in A
-tci = 0.081; % temperature coefficient of Isc in 1/°C
-tcv = -0.37; % temperature coefficient of Voc in 1/°C
+% NOTES:
+% 1. The lookup table can be ploted with the "plot_Isurf.m" script (tested with Octave 7.3.0)
+% 2. to print the lookup table contents use "look_into_binary_lut.m" script
 
-% Open module voltages
-vmin = 77.76; % minimum open module voltage in V - estimated Value: 90% * Vmax
-vmax = voc; % maximum open module voltage in V
-amin = 200; % minimum solar radiation in W/m^2  - 200W/m² as recommanded in the Boeke Paper
-amax = astc; % maximum solar radiation in W/m^2
-tmin = -25;
-tmax = 75;
+%%% PV MODULE DATA %%%
+n = 4;              % number of modules in series
+voc = 21.6;         % open circuit voltage in V
+isc = 6.14;         % short circuit current in A
+vmpp = 17.6;        % voltage at maximum power point in V
+impp = 5.58;        % current at maximum power point in A
+vmin = 0.9*n*voc;   % minimum open module voltage in V - in this case: not listed in datasheet -> estimate Value: 90% * Vmax
+tci = 0.081;        % temperature coefficient of Isc in 1/°C
+tcv = -0.37;        % temperature coefficient of Voc in 1/°C
+
+%%% LUT VARIABLES %%%
+tn = 25;        % nominal temperature in °C
+tmin = -25;     % minimum temperature in °C
+tmax = 75;      % maximum temperature in °C
+astc = 1000;    % solar radiation at STC in W/m^2
+amin = 200;     % minimum solar radiation in W/m^2  - 200W/m² as recommanded in the Boeke Paper
+amax = astc;    % maximum solar radiation in W/m^2
+vmpp *= n;      % voltage at maximum power point in V
+vmax = n*voc;   % maximum open module voltage in V
+
+b = 0.09;       % fit parameter (adjust these according to the instructions in the Boeke Paper)
+
+vstep = 101;    % number of voltage steps
+astep = 11;     % number of radiation steps
+tstep = 21;     % number of temperature steps
 
 % Ranges
-a = linspace(0, amax, astep); % solar radiations in W/m^2
-t = linspace(tmin,tmax,tstep); % temperatures in °C
-v = linspace(0, vmax, vstep); % voltages in V
+a = linspace(0, amax, astep);   % solar radiations in W/m^2
+t = linspace(tmin,tmax,tstep);  % temperatures in °C
+v = linspace(0, vmax, vstep);   % voltages in V
 
 %%% FUNCTIONS %%%
 
